4675d990b274b718600cca601964e5d11667ea71
orang sawah <najwanurhasanah230@gmail.com>
2026-06-05 17:02:10 +0600

    feat: create all database migrations

---
+++ database/migrations/2026_06_05_105746_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->string('midtrans_order_id')->unique();
+            $table->string('snap_token')->nullable();
+            $table->decimal('amount', 10, 2);
+            $table->enum('status', ['pending', 'paid', 'failed', 'expired'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
